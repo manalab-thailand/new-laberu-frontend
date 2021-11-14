ae63d1134553d069fbd95497834563e835e557c1
Doublepor <doublepor.pp@gmail.com>
2021-11-14 20:33:13 +0700

    integration classfication

--- src/pages/laberu/classification/classification.vue
+++ src/pages/laberu/classification/classification.vue
@@ -3,49 +3,143 @@
     <div class="col">
       <ClassificationHeader />
       <div class="row justify-center q-mb-lg">
-        <div
+        <!-- <div
           class="sub-image-wrapper"
           :style="{ backgroundImage: `url(images/gridbox.png)` }"
+        ></div> -->
+        <div
+          class="sub-image-wrapper"
+          :style="{ backgroundImage: `url(${image_url})` }"
         ></div>
       </div>
-      <q-card flat bordered class="q-mt-md bg-grey-2">
-        <q-card-section
-          >Submitted form contains the following formData (key =
-          value):</q-card-section
-        >
-        <q-separator />
-        <q-card-section class="row q-gutter-sm items-center">
-          <!-- <div
-            v-for="(item, index) in submitResult"
-            :key="index"
-            class="
-              q-px-sm q-py-xs
-              bg-grey-8
-              text-white
-              rounded-borders
-              text-center text-no-wrap
-            "
-          >
-            {{ item.label }} = {{ item.value }}
-          </div> -->
-        </q-card-section>
-      </q-card>
     </div>
-    <div class="col"><ClassificationSidebar /></div>
+    <div class="col">
+      <ClassificationSidebar
+        :project="project"
+        :imageData="imageData"
+        @onSave="onSave($event)"
+        @onSkip="initState()"
+      />
+    </div>
   </div>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
+<script lang='ts'>
+import { defineComponent, ref, computed, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import ClassificationSidebar from "src/pages/laberu/classification/ClassificationSidebar.vue";
 import ClassificationHeader from "pages/laberu/classification/ClassificationHeader.vue";
+import { useStore } from "src/store";
+import { useQuasar } from "quasar";
+import { ExecException } from "child_process";
+
+interface IEventResult {
+  value: boolean;
+  label: string;
+}
+
 export default defineComponent({
   components: {
     ClassificationSidebar,
     ClassificationHeader,
   },
   setup() {
-    return {};
+    const route = useRoute();
+    const router = useRouter();
+    const store = useStore();
+    const q = useQuasar();
+
+    const project_id = route.query.project_id as string;
+
+    const project = computed(() =>
+      store.state.moduleProjects.projects.find(
+        (project) => project._id == project_id
+      )
+    );
+
+    const user = computed(() => store.state.moduleAuth.user);
+    const taskImage = computed(() => store.state.moduleTaskImage.task_image);
+    const imageData = computed(() => store.state.moduleTaskImage.image_data);
+
+    const image_url = ref<string>();
+    const startedAt = ref<Date>();
+
+    const initState = async () => {
+      try {
+        q.loading.show();
+
+        const resp = await store.dispatch("moduleTaskImage/getTaskImage", {
+          user_id: user.value._id,
+          project_id: project.value!._id,
+        });
+
+        if (!resp) {
+          q.notify({
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "Task image not avaliable for you",
+          });
+          router.go(-1);
+          return;
+        }
+
+        startedAt.value = new Date();
+        image_url.value = `${project.value?.base_image_url}/${taskImage.value.shortcode}.${project.value?.image_type}`;
+      } catch (error) {
+        throw new Error((error as ExecException).message);
+      } finally {
+        q.loading.hide();
+      }
+    };
+
+    onMounted(async () => {
+      try {
+        await initState();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    const onSave = async (result: IEventResult) => {
+      const taskSuccess = ref({
+        shortcode: taskImage.value.shortcode,
+        result: {
+          classification: {
+            class: result,
+          },
+        },
+        task_id: taskImage.value._id,
+        user_id: user.value._id,
+        project_id: project.value?._id,
+        custom: {
+          user_id: user.value._id,
+          group_id: null,
+        },
+        startedAt: startedAt.value,
+      });
+
+      try {
+        q.loading.show();
+        await store.dispatch(
+          "moduleTaskSuccess/createTaskSuccess",
+          taskSuccess.value
+        );
+        await initState();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        q.loading.hide();
+      }
+    };
+
+    return {
+      project,
+      imageData,
+      image_url,
+      initState,
+      onSave,
+    };
   },
 });
 </script>
